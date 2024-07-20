18e8f246b2b96e904bec6f89a0b98cdd9d2f1824
karan <vishal.k.gvtechnolab@gmail.com>
2024-07-20 16:22:17 +0530

    enum

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,34 @@
-/** tuple :- fix length array where each element has particular type */
-
-/** we work with when we use pair of values */
-
-/** simple tuple */
-// let user: [number, string] = [0, "vishal"];
-
-/** If we add the third element they give error */
-
-// let user: [number, string] = [0, "vishal", 69];
-let user: [number, string] = [0, "vishal"];
-
-/** one thing remember about the tuple is internally it's represent the javascript array */
-
-/** use intelligence like "user[0]." and "user[1]."  */
-
-/** We also use of all method and property of array like "user." but here only one issue is we can use "PUSH" method to add element to fix array.. this is one of the reasone that solve by typescript  */
-
-user.push(5);
-
-/** As best practise restrict tuples only two values because any more than that it's make code hard to understand for example */
-
-let user1: [number, string, boolean, number] = [1, "vishal", true, 0];
-
-/** What is value of this user1 will be represent it's relly hard to tell
+/** Typescript have built in another type "enum"
+ *  enum represent list of related constant
  *
+ *  Consider the we can represent size of tshirts as constant one way we can solve like
  *
- * Tuples are useful when only two values like key value pairs
+ * const small = 1,
+ * const medium= 2,
+ * const large = 3
+ * another way we can group this constant inside in 'enum'
  */
+
+//pascalCase
+enum Size {
+  Small = 1, //default compiler give value to small=0,medium=1,Large=2
+  Medium,
+  Large,
+}
+
+// we can use this enum using new variable
+
+let mySize: Size = Size.Medium;
+
+console.log(mySize);
+
+/** see the index.js file where generated js code is to verbose.
+ *
+ * we can generate more optimize code using below code like
+ */
+
+// const enum Size {
+//   Small = 1,
+//   Medium,
+//   Large,
+// }
