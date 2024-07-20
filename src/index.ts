6ac00953dbc6c133f7985abf2c76d37a77831c4e
karan <vishal.k.gvtechnolab@gmail.com>
2024-07-20 14:51:45 +0530

    Built-in types

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,27 @@
-let age: number = 20;
+/** primite type of javascript is
+ * 1) number 2) string 3)boolean 4) undefined 5) object 6)symbol
+ * it's work in typescript and also typescript add some
+ * 1) any 2) unknown 3) never 4)enum 5) tuple
+ */
 
-if (age < 50) age += 10;
+// play with primite type of typscript
 
-console.log(age);
+// let sales: number = 123_343_433;
+// let course:string = 'Typscript';
+// let is_published:boolean = true;
+
+/** In typescript we don't annoted variables because
+ * typescript compiler can detect which type of variables base
+ * on values.
+ *
+ * check below code.. hover mouse on variables and see compiler
+ * detect which type of variable
+ */
+
+let sales = 123_343_433;
+let course = "Typscript";
+let is_published = true;
+
+// if declare a variable only and hover it and see what is type
+
+let level;
