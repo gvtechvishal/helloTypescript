eff4e12f18305e3a4d25f726d9f33cc692d0a308
karan <vishal.k.gvtechnolab@gmail.com>
2024-07-20 15:16:05 +0530

    Learn about any type

--- src/index.ts
+++ src/index.ts
@@ -25,3 +25,34 @@ let is_published = true;
 // if declare a variable only and hover it and see what is type
 
 let level;
+
+/** typscript compiler assign any type if we not annoted.
+ * It's lose the benifite of typescript.
+ * best practice avoid to use.
+ */
+
+level = 1;
+level = "hiii";
+
+//Another example of for learn
+
+// function render(document) {
+//   console.log(document);
+// }
+
+/** Look and hover of render fn parameter document
+ * it's show compilation error(document is any type)
+ */
+
+/** consider one scenario when we convert our javascript code
+ * into typescript. impossible for us to which type of parameter we receive in this type functions where we use "any" type
+ *
+ * we solve this error two way
+ * 1) annotated parameter like render(document:any)
+ * what happne when lot's of error in the code we don't go everywhere and annotated like this
+ * 2) We can disable this type of error configuring typscript compiler in tsconfing.json
+ */
+
+function render(document) {
+  console.log(document);
+}
