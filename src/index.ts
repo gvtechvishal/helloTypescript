783bc0fb3632b264190e1bd336a3c53e0aabb440
karan <vishal.k.gvtechnolab@gmail.com>
2024-07-24 12:53:36 +0530

    unknown type

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,29 @@
-/** Type assertions:-
+/** How to work unknown type..
  *
+ * we know "any" type and avoid as a possible.
  *
- * Some time we know more about type of object then typescript..
+ * let consider where we converting javascript to typescript code and
+ * we have situation where we use any type.
  *
- * here real example
+ * see the below example
  */
 
-let phone = document.getElementById("phone");
+function render(document: any) {
+  // we code like
+  document.move();
+  document.fly();
+  document.whateverWeWant();
+  // here not type cheking..
+  /** if our program is run document not contain any of the method
+   * our program crash so insted applying "any" type we use "unknown".. */
+}
 
-/**
-  They return "HTMLElement" type of object right..
-  
-  --> but we not access value propert.
-  
-  Upper line of code consider we get input from input element and that element 
-  type "HTMLInputElement" and this type of objects have value property right...
-  
-  we know more about type of object then typscript...
-  so using type assertion we tell the typescript compiler which type of object are
-  */
+function render1(document: unknown) {
+  // we use type narrowing
 
-let phone1 = document.getElementById("phone") as HTMLInputElement;
-//second type of syntax
-let phone2 = <HTMLInputElement>document.getElementById("phone");
-
-/** now we can access the value property like.. */
-
-console.log(phone1.value);
-
-/** NOTE:- IF getElementById('phone') return object is not HTMLInputElement object 
-  
- let phone1 = document.getElementById("phone") as HTMLInputElement;
-
- Upper line not generate any exception  and we acess the propety like
-
- phone1.value 
-
- out program will be carsh....
- BE AWARE OF THIS........................
-*/
+  if (document instanceof wordDocument) {
+    document.move();
+  }
+  document.fly();
+  document.whateverWeWant();
+}
