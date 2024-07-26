0fffaf9243c103e5bf3d153a95fa3eae4cb50552
karan <vishal.k.gvtechnolab@gmail.com>
2024-07-26 11:18:57 +0530

    Creating class in typescript

--- dist/index.js
+++ dist/index.js
@@ -1,25 +1,15 @@
 "use strict";
-var _a;
-function getCustomer(id) {
-    return id === 0
-        ? null
-        : {
-            birthday: new Date(),
-        };
+class Account {
+    constructor(id, user, balance) {
+        this.id = id;
+        this.user = user;
+        this.balance = balance;
+    }
+    deposit(amount) {
+        if (amount <= 0) {
+            throw new Error("Enter valide amount");
+        }
+        this.balance += amount;
+    }
 }
-let customer = getCustomer(0);
-if (customer !== null && customer !== undefined)
-    console.log(customer.birthday);
-console.log(customer === null || customer === void 0 ? void 0 : customer.birthday);
-function getCustomer2(id) {
-    return id === 0
-        ? null
-        : {
-            birthday: new Date(),
-        };
-}
-let customer2 = getCustomer2(1);
-console.log((_a = customer2 === null || customer2 === void 0 ? void 0 : customer2.birthday) === null || _a === void 0 ? void 0 : _a.getFullYear());
-let log = null;
-console.log(log === null || log === void 0 ? void 0 : log("a"));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,24 @@
-/** never type::--  which represent values that never occure
- *
- * this type not use quite often...
- *
- * let see some examples...
- */
+/** creating the class:- class is blue print of object or factory for creating the object */
 
-/**
- * we define function that process the events continuously.
- * constantly watching a message queue waiting for next events
- */
+class Account {
+  // properties
+  id: number;
+  user: string;
+  balance: number;
 
-function processEvents(): never {
-  while (true) {
-    // Read the msg from a queue
+  constructor(id: number, user: string, balance: number) {
+    this.id = id;
+    this.user = user;
+    this.balance = balance;
+  }
+
+  // methode
+
+  deposit(amount: number) {
+    if (amount <= 0) {
+      throw new Error("Enter valide amount");
+    }
+
+    this.balance += amount;
   }
 }
-
-processEvents();
-
-/** after calling the function we known that below line never executing because function
- * never returns because we have infinet loop..
- *
- * so we have apply return type "never" to function tell the compiler this function never returns..
- *
- * after the give return type "never" to function below line blur see
- */
-console.log("Hello world");
-
-/** This is benefit of use never type that detect the unreachable code but
- * we configure the tsconfig.json file
- */
-
-/** see the below example aslo like  */
-
-function reject(message: string): never {
-  throw new Error(message);
-}
-
-/** If we call like
- *
- * reject(.....);
- * console.log('hellow world')
- *
- * after the calling function we dont reach below code
- *
- * that why we use type never that show unreachable code.
- */
