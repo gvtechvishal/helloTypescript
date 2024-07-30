db1b8f0eb0cbb48cc8c06c9b2bc0a5cd2107b61f
karan <vishal.k.gvtechnolab@gmail.com>
2024-07-30 13:02:49 +0530

    abstract class and methods

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,40 @@
-/** protected :- Protected member access anywhere within class but not outside but differnce is protected members are inherited but private not.  */
+/** Abstract classes and methods
+ *
+ * Consider this example where we bulild simple paint app..that allow to draw circle,rectangle,triangle on canvas.
+ *
+ * So every shape have common properties like color,size,position
+ *
+ */
 
-class Person {
-  constructor(public firstName: string, public lastName: string) {}
+abstract class Shape {
+  constructor(public color: string) {}
+  // define render method
 
-  get fullName() {
-    return this.firstName + " " + this.lastName;
+  abstract render(): void;
+  // at this point we dont know how to render a shape because rendering algorithm depend on type of shape..
+}
+
+class Circle extends Shape {
+  constructor(public radius: number, color: string) {
+    super(color);
   }
 
-  protected walk() {
-    console.log("walking");
+  // this method for draw circle
+  override render(): void {
+    console.log("Rendering a circle");
   }
 }
 
-class Student extends Person {
-  constructor(public studentId: number, firstName: string, lastName: string) {
-    super(firstName, lastName);
-  }
+/** one problem in this implementation we create Shape objects */
 
-  takeTest() {
-    //we call protected member here or any where in this class
-    this.walk();
-    console.log("Taking a test");
-  }
-}
+// let shape = new Shape("red");
+// shape.render();
+
+/** This make not any sense render a shape without what type of shape right..because shape is not real things like a circle.
+ *  That why we use Abstract classes and methods...it is stop us being able to creating a instance of shape class we mark the class as
+ * abstract..
+ *
+ * we use to "abstract" keyword for telling the compiler this is class shape is abstract.  not ready so another class like circle extended..
+ *
+ * abstract class have abstract methods also that are methods that no implementation.. so we prefix the method with abstract
+ */
