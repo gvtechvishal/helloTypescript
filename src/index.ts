d8b8b65ca3893e30f935f708a402dd64ed84efd2
karan <vishal.k.gvtechnolab@gmail.com>
2024-07-31 15:00:28 +0530

    Generic classes

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,20 @@
-/** -------------------------:Generics:---------------------- */
-/** Problem that solve by generic:--
- *
- * class that represent key value pair
- */
+/** --------------: Generic class for solving to problem:--------------- */
 
-class keyValuePair {
-  constructor(public key: number, public value: string) {}
+//we write one more generic types
+class keyValuePair<T> {
+  constructor(public key: T, public value: string) {}
 }
 
-// here we only pass number as a key what we do for pass string as well as
+// we can pass like:--
 
-let pair = new keyValuePair(1, "vishal");
+const pair = new keyValuePair<number>(1, "vishal");
+const pair2 = new keyValuePair<string>("2", "kanti");
+//compiler auto detect type of key
+const pai3 = new keyValuePair(1, "vishal");
 
-//below line of code not working
-// let pair2 = new keyValuePair("2", "nishant");
-
-//pair2.key.  we don't see the intelegence here
-
-/** so we can solve using any type to key parameter right but is not right way to solve this type of problems..
- *
- * another way we create anthor class for that accept key as string like below
- */
-
-class StringKeyValuePair {
-  constructor(public key: string, public value: string) {}
+class keyValuePair2<K, V> {
+  constructor(public key: K, public value: V) {}
 }
-//this line is valid now
-// let pair2 = new keyValuePair("2", "nishant");
 
-/** How to solve this problem: we create generic or common solution for this. */
+//compiler auto detect
+const pair4 = new keyValuePair2("5", "vish");
