c9cd36d13eff0ae1fd95a730a0bfec67f66b5851
karan <vishal.k.gvtechnolab@gmail.com>
2024-08-05 14:21:14 +0530

    Methods decorators

--- dist/index.js
+++ dist/index.js
@@ -5,16 +5,22 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-function Component(constructor) {
-    console.log("Component decorator called");
-    constructor.prototype.uniqueId = Date.now();
-    constructor.prototype.insertInDom = () => {
-        console.log("Inserting the component in the DOM");
+function Log(target, methodName, descriptor) {
+    const original = descriptor.value;
+    descriptor.value = function (message) {
+        console.log("Before");
+        original.call(this, message);
+        console.log("After");
     };
 }
-let ProfileComponent = class ProfileComponent {
-};
-ProfileComponent = __decorate([
-    Component
-], ProfileComponent);
+class Person {
+    say(message) {
+        console.log("Person says " + message);
+    }
+}
+__decorate([
+    Log
+], Person.prototype, "say", null);
+let person = new Person();
+person.say("Hello");
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,28 @@
-/** Decorators composition*/
+/** Method Decorators:-- */
 
-type ComponentOptions = {
-  selector: string;
-};
+function Log(target: any, methodName: string, descriptor: PropertyDescriptor) {
+  // we do completelly replace say() method with new method like below code
+  // descriptor.value = function () {
+  //   console.log("new implementation");
+  // };
 
-function Component(options: ComponentOptions) {
-  return (constructor: Function) => {
-    console.log("Component decorator called");
-    constructor.prototype.options = options;
-    constructor.prototype.uniqueId = Date.now();
-    constructor.prototype.insertInDom = () => {
-      console.log("Inserting the component in the DOM");
-    };
+  const original = descriptor.value as Function;
+  descriptor.value = function (message: string) {
+    //below line of code make more generic so we can use other type also
+    // descriptor.value = function (...args: any) {
+    console.log("Before");
+    //call original method.. first create reference of original method
+    // original.call(this, ...args);
+    original.call(this, message);
+    console.log("After");
   };
 }
-
-function Pipe(constructor: Function) {
-  console.log("Pipe decorator called");
-  constructor.prototype.pipe = true;
+class Person {
+  @Log
+  say(message: string) {
+    console.log("Person says " + message);
+  }
 }
-@Component({ selector: "#my-profile" })
-@Pipe // call first and then pass to @Component
-class ProfileComponent {}
+
+let person = new Person();
+person.say("Hello");
