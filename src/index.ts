d82d81d235b26c719b8c68365b36b6579c9c3e2a
karan <vishal.k.gvtechnolab@gmail.com>
2024-08-05 12:31:28 +0530

    class decorator

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,14 @@
-/** Decorators:-  Decorators in TypeScript are a special kind of declaration that can be attached to a class, method, accessor, property, or parameter. Decorators are used to modify or enhance these elements. They are a feature proposed for JavaScript and are available as an experimental feature in TypeScript. 
- * 
- * Types of Decorators
-1) Class Decorators
-2) Method Decorators
-3) Accessor Decorators
-4) Property Decorators
-5) Parameter Decorators
- */
+/** Class decorators */
 
-function component(
-  target: typeof ProfileComponent
-): void | typeof ProfileComponent {
-  throw new Error("Function not implemented.");
+function Component(constructor: Function) {
+  console.log("Component decorator called");
+
+  // add extra property to ProfileComponent class via prototype
+  constructor.prototype.uniqueId = Date.now();
+  constructor.prototype.insertInDom = () => {
+    console.log("Inserting the component in the DOM");
+  };
 }
 
-@component
+@Component
 class ProfileComponent {}
