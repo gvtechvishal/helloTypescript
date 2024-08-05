79b2b9452ad10b67b91f2dfa049f360ac42851e2
karan <vishal.k.gvtechnolab@gmail.com>
2024-08-05 14:59:32 +0530

    parameter decoratores

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,20 @@
-/** Property decorator */
+/** Parameter Decorator */
 
-function MinLength(length: number) {
-  // Here return actully decorator function
-  // property decoratore very similary to method decorator
+type Watchedparameter = {
+  methodName: string;
+  parameterIndex: number;
+};
 
-  return (target: any, propertyName: string) => {
-    let value: string;
+const watchedParameters: Watchedparameter[] = [];
 
-    const descriptor: PropertyDescriptor = {
-      get() {
-        return value;
-      },
-      set(newValue: string) {
-        if (newValue.length < length) {
-          throw new Error(
-            `${propertyName} should be at least ${length} characters long.`
-          );
-        }
-        value = newValue;
-      },
-    };
-
-    console.log(target, propertyName, descriptor);
-
-    Object.defineProperty(target, propertyName, descriptor);
-  };
+function Watch(target: any, methodName: string, parameterIndex: number) {
+  watchedParameters.push({
+    methodName,
+    parameterIndex,
+  });
+}
+class Vehicle {
+  move(@Watch speed: number) {}
 }
 
-class User {
-  //check password have minimum 4 length
-  @MinLength(4)
-  password: string;
-
-  constructor(password: string) {
-    this.password = password;
-  }
-}
-
-let user = new User("1234fd5");
-
-console.log(user.password);
+console.log(watchedParameters);
